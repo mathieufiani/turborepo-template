108b765524f1b8fc89fa08b39e4a803f5cc55275
mathieufiani <mathieu.fiani2@gmail.com>
2022-08-01 22:17:38 +0100

    initial commit

---
+++ apps/client/src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import '../../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const AnyComponent = Component as any;
+  return <AnyComponent {...pageProps} />;
+}
+
+export default MyApp
