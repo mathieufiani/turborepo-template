b21094b804ea1a701679338b37f921de9d31fb02
mathieufiani <mathieu.fiani2@gmail.com>
2022-08-04 23:11:24 +0100

    added taiwind

--- apps/server/dist/src/main.js
+++ apps/server/dist/src/main.js
@@ -14,7 +14,7 @@ const app_module_1 = require("./app.module");
 function bootstrap() {
     return __awaiter(this, void 0, void 0, function* () {
         const app = yield core_1.NestFactory.create(app_module_1.AppModule);
-        yield app.listen(3000);
+        yield app.listen(8000);
     });
 }
 bootstrap();
