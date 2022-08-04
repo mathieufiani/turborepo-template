e64f42c8c349bcd4ff90871db78f0515a89fa0eb
mathieufiani <mathieu.fiani@gmail.com>
2022-08-04 18:16:30 +0200

    adding tailwind generally

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
 export const Button = () => {
-  return <button>Boop</button>;
+  return <button type={'button'} className={"bg-blue-500 hover:bg"}>Boop</button>;
 };
