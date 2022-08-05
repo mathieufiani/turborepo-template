1780f7642e1b98d0e5986a2edea6d6850a5118bb
mathieufiani <mathieu.fiani@gmail.com>
2022-08-05 13:43:56 +0200

    should pass test

--- apps/admin/src/pages/_app.tsx
+++ apps/admin/src/pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from 'next/app'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const AnyComponent = Component as any;
-  console.log("supp ggg")
   return <AnyComponent {...pageProps} />;
 }
 
