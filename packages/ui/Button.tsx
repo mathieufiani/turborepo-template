56eb4ea2c8dff5762d646eb4b478f32c80294f70
mathieufiani <mathieu.fiani@gmail.com>
2022-08-05 13:33:51 +0200

    happy holidays

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -4,7 +4,7 @@ export const Button = () => {
   return (
     <button
       type={'button'}
-      className={'bg-blue-500 hover:bg-blue-700 h-16 w-16'}
+      className= {'bg-blue-500 hover:bg-blue-700 h-16 w-16'}
     >
       Boop
     </button>
