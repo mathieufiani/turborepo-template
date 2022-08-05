545d87873f4f49f3d1907e643633caced55fd2c9
mathieufiani <mathieu.fiani@gmail.com>
2022-08-05 13:36:04 +0200

    happy holidays

--- apps/admin/src/pages/_app.tsx
+++ apps/admin/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const AnyComponent = Component as any;
+  console.log("supp g")
   return <AnyComponent {...pageProps} />;
 }
 
